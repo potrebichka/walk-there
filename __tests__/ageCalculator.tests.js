6f1b513ce628f628f108c0e2e9d49b64cc6cac95
Nina Potrebich <potrebich@gmail.com>
2019-11-15 10:22:00 -0800

    add method getEarthAge

--- __tests__/ageCalculator.tests.js
+++ __tests__/ageCalculator.tests.js
@@ -1,12 +1,13 @@
 import { AgeCalculator } from '../src/ageCalculator';
 
 describe ('Age Calculator', () => {
-    let user1, user2, user3, user4;
+    let user1, user2, user3, user4, user5;
     beforeEach(() => {
         user1 = new AgeCalculator(new Date('07-27-1987'));
         user2 = new AgeCalculator(new Date('01-29-1951'));
         user3 = new AgeCalculator(new Date('10-05-2012'));
         user4 = new AgeCalculator(new Date('10-05-2032'));
+        user5 = new AgeCalculator(new Date());
     }) 
     test('should correctly create object Age Calculator', () => {
         expect(user1.birthday).toEqual(new Date('07-27-1987'));
@@ -15,11 +16,20 @@ describe ('Age Calculator', () => {
         expect(user4.birthday).toEqual(new Date('10-05-2032'));
     });
 
+    test('should correctly create object Age Calculator and calculate number of days', () => {
+        expect(user1.numberOfDays).toEqual(11799);
+        expect(user2.numberOfDays).toEqual(25127);
+        expect(user3.numberOfDays).toEqual(2597);
+        expect(user4.numberOfDays).toEqual(null);
+        expect(user5.numberOfDays).toEqual(0);
+    });
+
     test('should correctly return user age in Earth years', () => {
         expect(user1.getEarthAge()).toEqual(32);
         expect(user2.getEarthAge()).toEqual(68);
         expect(user3.getEarthAge()).toEqual(7);
         expect(user4.getEarthAge()).toEqual("You haven't born yet");
+        expect(user5.getEarthAge()).toEqual(0);
     });
 
     test('should correctly return user age in Mercury years', () => {
@@ -27,6 +37,7 @@ describe ('Age Calculator', () => {
         expect(user2.getMercuryAge()).toEqual(285);
         expect(user3.getMercuryAge()).toEqual(29);
         expect(user4.getMercuryAge()).toEqual("You haven't born yet");
+        expect(user5.getMercuryAge()).toEqual(0);
     });
 
     test('should correctly return user age in Venus years', () => {
@@ -34,6 +45,7 @@ describe ('Age Calculator', () => {
         expect(user2.getVenusAge()).toEqual(111);
         expect(user3.getVenusAge()).toEqual(11);
         expect(user4.getVenusAge()).toEqual("You haven't born yet");
+        expect(user5.getVenusAge()).toEqual(0);
     });
 
     test('should correctly return user age in Mars years', () => {
@@ -41,6 +53,7 @@ describe ('Age Calculator', () => {
         expect(user2.getMarsAge()).toEqual(36);
         expect(user3.getMarsAge()).toEqual(3);
         expect(user4.getMarsAge()).toEqual("You haven't born yet");
+        expect(user5.getMarsAge()).toEqual(0);
     });
 
     test('should correctly return user age in Jupiter years', () => {
@@ -48,6 +61,7 @@ describe ('Age Calculator', () => {
         expect(user2.getJupiterAge()).toEqual(5);
         expect(user3.getJupiterAge()).toEqual(0);
         expect(user4.getJupiterAge()).toEqual("You haven't born yet");
+        expect(user5.getJupiterAge()).toEqual(0);
     });
 
     test('should correctly return user age in Saturn years', () => {
@@ -55,6 +69,7 @@ describe ('Age Calculator', () => {
         expect(user2.getSaturnAge()).toEqual(2);
         expect(user3.getSaturnAge()).toEqual(0);
         expect(user4.getSaturnAge()).toEqual("You haven't born yet");
+        expect(user5.getSaturnAge()).toEqual(0);
     });
 
     test('should correctly return user age in Uranus years', () => {
@@ -62,6 +77,7 @@ describe ('Age Calculator', () => {
         expect(user2.getUranusAge()).toEqual(0);
         expect(user3.getUranusAge()).toEqual(0);
         expect(user4.getUranusAge()).toEqual("You haven't born yet");
+        expect(user5.getUranusAge()).toEqual(0);
     });
 
     test('should correctly return user age in Neptune years', () => {
@@ -69,6 +85,7 @@ describe ('Age Calculator', () => {
         expect(user2.getNeptuneAge()).toEqual(0);
         expect(user3.getNeptuneAge()).toEqual(0);
         expect(user4.getNeptuneAge()).toEqual("You haven't born yet");
+        expect(user5.getNeptuneAge()).toEqual(0);
     });
 
     test('should correctly return user age in Pluto years', () => {
@@ -76,5 +93,6 @@ describe ('Age Calculator', () => {
         expect(user2.getPlutoAge()).toEqual(0);
         expect(user3.getPlutoAge()).toEqual(0);
         expect(user4.getPlutoAge()).toEqual("You haven't born yet");
+        expect(user5.getPlutoAge()).toEqual(0);
     });
 })
--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -1,13 +1,19 @@
 export class AgeCalculator {
     constructor(date) {
         this.birthday = date;
-    }
+        this.numberOfDays = (new Date() >= date) ? parseInt((new Date().getTime() - date.getTime())/(1000*3600*24)) : null;
+    };
 
     getMercuryAge() {
-        let mercuryAge = 
-    }
+        //let mercuryAge = 
+    };
 
     getEarthAge() {
-
+        const earthOrbitPeriod = 365.26;
+        if (this.numberOfDays || this.numberOfDays === 0) {
+            return parseInt(this.numberOfDays / earthOrbitPeriod);
+        }
+        return "You haven't born yet"
+        
     }
 }
