2c34ff67e780122b8712d6b4ee10fd2b87074f29
Nina Potrebich <potrebich@gmail.com>
2019-11-22 10:08:12 -0800

    add list of specialities

--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@ import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { ListOfSpecialities } from './specialities';
 
 $(function() {
+    let location;
+    $("#locationForm").submit(function(event) {
+        event.preventDefault();
+        location = $("#userLocationInput").val();
+        
+        (async () => {
+            let listOfSpecialities = new ListOfSpecialities();
+            const response = await listOfSpecialities.getListOfSpecialities();
+            showListOfSpecialities(response.data);
+        })();
+    });
 
-    
+    function showListOfSpecialities(response) {
+        for (let i=0; i<response.length; i++) {
+            $("#symptomsUserSelect").append(`<option>${response[i].uid}</option>`);
+        }
+        $(".afterGettingLocation").show();
+    }
 });
